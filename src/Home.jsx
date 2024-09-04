2b2a2a846c5908150e5458decd8621bdf14f32e5
Yousief Sameh <yousief.sameh@outlook.com>
2024-09-04 03:14:01 +0300

    Add Responsive

--- src/Home.jsx
+++ src/Home.jsx
@@ -4,15 +4,17 @@ import Navbar from './Navbar';
 const Home = () => {
   return (
     <header className='bg-third h-screen'>
-      <div className="container mx-auto">
+      <div className="container mx-auto px-4 md:px-8">
         <Navbar/>
-        <div className="hero flex items-center justify-around">
-          <div className="text flex flex-col gap-16">
-            <h1 className="text-7xl font-bold font-primary">We Tell Stories <br /> With Flowers</h1>
-            <button className='border border-black px-12 py-4 w-fit rounded-full hover:bg-black hover:text-white transition-colors'><a href="#" className='text-inherit uppercase'>Shop Now</a></button>
+        <div className="hero flex flex-col-reverse md:flex-row items-center justify-around">
+          <div className="text flex flex-col gap-8 md:gap-16 text-center md:text-left mt-8 md:mt-0">
+            <h1 className="text-4xl md:text-5xl lg:text-7xl font-bold font-primary">We Tell Stories <br /> With Flowers</h1>
+            <button className='border border-black px-8 md:px-12 py-3 md:py-4 w-fit self-center md:self-start rounded-full hover:bg-black hover:text-white transition-colors'>
+              <a href="#" className='text-inherit uppercase'>Shop Now</a>
+            </button>
           </div>
-          <div className="image w-1/3">
-            <img src="Images\img03.png" alt="" className='w-full' />
+          <div className="image w-2/3 md:w-1/3">
+            <img src="Images/img03.png" alt="" className='w-full' />
           </div>
         </div>
       </div>
@@ -20,4 +22,4 @@ const Home = () => {
   );
 }
 
-export default Home
+export default Home;
--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,31 +1,35 @@
-import React from 'react'
+import React from 'react';
 
 const Navbar = () => {
   return (
-    <nav>
-      <div className='flex items-center justify-between'>
-        <input type="text" placeholder='Search' className='bg-transparent p-3 placeholder:text-black placeholder:uppercase'/>
-        <h1 className='font-primary text-5xl p-2 font-bold'>Fiore</h1>
-        <div className="account flex gap-6 items-center">
-          <a href="#" className='uppercase'>account</a>
-          <a href="#"><i className="fa-regular fa-heart fa-xl hover:text-red-600 transition-all"></i></a>
+    <nav className='p-4'>
+      <div className='flex flex-col md:flex-row items-center justify-between'>
+        <input 
+          type="text" 
+          placeholder='Search' 
+          className='bg-transparent p-3 placeholder:text-black placeholder:uppercase w-full md:w-auto mb-4 md:mb-0' 
+        />
+        <h1 className='font-primary text-3xl md:text-5xl p-2 font-bold text-center md:text-left'>Fiore</h1>
+        <div className="account flex gap-4 md:gap-6 items-center">
+          <a href="#" className='uppercase text-sm md:text-base'>Account</a>
+          <a href="#"><i className="fa-regular fa-heart fa-lg md:fa-xl hover:text-red-600 transition-all"></i></a>
           <div className='relative'>
-            <a href="#" className=''><i className="fa-solid fa-bag-shopping fa-xl"></i></a>
-            <span className='absolute -top-3 -right-4 w-6 h-6 bg-secondary rounded-full flex items-center justify-center text-white'>0</span>
+            <a href="#" className=''><i className="fa-solid fa-bag-shopping fa-lg md:fa-xl"></i></a>
+            <span className='absolute -top-2 md:-top-3 -right-3 md:-right-4 w-5 md:w-6 h-5 md:h-6 bg-secondary rounded-full flex items-center justify-center text-white text-xs md:text-sm'>0</span>
           </div>
         </div>
       </div>
-      <div className='flex gap-6 items-center justify-center py-4'>
-        <a href="#" className='uppercase before:absolute relative before:h-0.5 before:w-0 hover:before:w-full before:bg-secondary before:bottom-0 before:transition-all'>Home</a>
-        <a href="#" className='uppercase before:absolute relative before:h-0.5 before:w-0 hover:before:w-full before:bg-secondary before:bottom-0 before:transition-all'>Flowers</a>
-        <a href="#" className='uppercase before:absolute relative before:h-0.5 before:w-0 hover:before:w-full before:bg-secondary before:bottom-0 before:transition-all'>Plants</a>
-        <a href="#" className='uppercase before:absolute relative before:h-0.5 before:w-0 hover:before:w-full before:bg-secondary before:bottom-0 before:transition-all'>Services</a>
-        <a href="#" className='uppercase before:absolute relative before:h-0.5 before:w-0 hover:before:w-full before:bg-secondary before:bottom-0 before:transition-all'>Floral Classes</a>
-        <a href="#" className='uppercase before:absolute relative before:h-0.5 before:w-0 hover:before:w-full before:bg-secondary before:bottom-0 before:transition-all'>Our Story</a>
-        <a href="#" className='uppercase before:absolute relative before:h-0.5 before:w-0 hover:before:w-full before:bg-secondary before:bottom-0 before:transition-all'>Blog</a>
+      <div className='flex flex-wrap gap-4 md:gap-6 items-center justify-center py-4'>
+        <a href="#" className='uppercase text-sm md:text-base before:absolute relative before:h-0.5 before:w-0 hover:before:w-full before:bg-secondary before:bottom-0 before:transition-all'>Home</a>
+        <a href="#" className='uppercase text-sm md:text-base before:absolute relative before:h-0.5 before:w-0 hover:before:w-full before:bg-secondary before:bottom-0 before:transition-all'>Flowers</a>
+        <a href="#" className='uppercase text-sm md:text-base before:absolute relative before:h-0.5 before:w-0 hover:before:w-full before:bg-secondary before:bottom-0 before:transition-all'>Plants</a>
+        <a href="#" className='uppercase text-sm md:text-base before:absolute relative before:h-0.5 before:w-0 hover:before:w-full before:bg-secondary before:bottom-0 before:transition-all'>Services</a>
+        <a href="#" className='uppercase text-sm md:text-base before:absolute relative before:h-0.5 before:w-0 hover:before:w-full before:bg-secondary before:bottom-0 before:transition-all'>Floral Classes</a>
+        <a href="#" className='uppercase text-sm md:text-base before:absolute relative before:h-0.5 before:w-0 hover:before:w-full before:bg-secondary before:bottom-0 before:transition-all'>Our Story</a>
+        <a href="#" className='uppercase text-sm md:text-base before:absolute relative before:h-0.5 before:w-0 hover:before:w-full before:bg-secondary before:bottom-0 before:transition-all'>Blog</a>
       </div>
     </nav>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
